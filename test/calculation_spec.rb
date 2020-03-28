229fc027e1b843e17ac8e208c6451d8b5cf2e94f
hauhau <hauhau_sakura@icloud.com>
2020-03-28 19:12:47 +0900

    パス修正

--- test/calculation_spec.rb
+++ test/calculation_spec.rb
@@ -1,10 +1,10 @@
 # ./calculation_spec.rb
 
 require 'minitest/autorun'
-require './calculation'
+require '../calculation'
 
 class CalculationTest < Minitest::Test
   def test_sum_２つの引数の足し合わせた結果を返すこと
-    assert_equal(Calculation.sum(1, 4), 3)
+    assert_equal(Calculation.sum(1, 2), 3)
   end
 end
